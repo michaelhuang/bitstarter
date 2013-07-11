caf920f110c74a514620e8d664bab3cc7830800e
heissenberg <michaelhuang12@gmail.com>
2013-07-11 12:34:06 +0800

    test

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  console.log("request");
+  var content = fs.readFileSync("index.html","utf8");
+  response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
